e02207f82b5520349ea393d2096e13ef5005934c
MaryMoroz <morozova.mary@gmail.com>
2024-01-02 13:20:12 -0500

    added exactButton in class BaseLocator

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,7 +5,7 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 public class HomePage extends BaseLocator {
-    public Locator studyThisButton = button("Study This");
+    public Locator studyThisButton = exactButton("Study This");
 
     public HomePage(Page page, Playwright playwright) {
         super(page, playwright);
