b1cb6f7e664b7c8ef49946981ffc0ebb13c56ec8
Liudmila Plucci <112190395+liuda33@users.noreply.github.com>
2024-01-14 03:18:42 +0000

    1

---
+++ src/test/java/pages/MnemonicCardListPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class MnemonicCardsPage extends SideMenuPage{
+
+    protected MnemonicCardsPage(Page page, Playwright playwright) {
+        super(page, playwright);
+    }
+}
