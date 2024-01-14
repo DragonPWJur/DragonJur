4931cce47e7d386df0e9f0a9ba52fa96c6483948
laysan <laysansh@gmail.com>
2024-01-14 14:57:03 -0700

    TC1350-01 User can mark the card

--- src/test/java/tests/TestListTest.java
+++ src/test/java/tests/TestListTest.java
@@ -1,14 +1,10 @@
 package tests;
 
-import com.microsoft.playwright.Locator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
-import pages.PreconditionPage;
-import pages.TestTutorPage;
 import pages.TestsPage;
 import utils.TestData;
-import utils.TestUtils;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static utils.ProjectProperties.BASE_URL;
@@ -31,18 +27,5 @@ public class TestListTest extends BaseTest {
         assertThat(getPage()).hasURL(BASE_URL + TestData.TEST_TUTOR_END_POINT);
 //        assertThat(testsPage.getTestQuestion()).containsText("?");
         Assert.assertTrue(testsPage.countTestRadioButtons() >= 1);
-    }
-
-    @Test
-    public void testAfterMarkingTheCardTheNumberOfMarkedCardsIncreasedBy1() {
-        PreconditionPage preconditionPage = new PreconditionPage(getPage(), getPlaywright());
-        preconditionPage.resetCourseResults();
-        preconditionPage.startTest(TestData.ONE);
-
-        Locator removeFromMarkedButton = new TestTutorPage(getPage(), getPlaywright())
-                .clickMarkForReviewButton()
-                .getRemoveFromMarkedButton();
-
-        assertThat(removeFromMarkedButton).isVisible();
     }
 }
