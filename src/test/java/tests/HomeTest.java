55f437db9a5dc0233a364d31b0c5d60e44f27a38
MaryMoroz <morozova.mary@gmail.com>
2024-01-13 21:18:43 -0500

    made waiter=2000

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -72,7 +72,7 @@ public class HomeTest extends BaseTest {
         homePage
                 .clickWeek1FirstCheckbox();
 
-        getPage().waitForTimeout(3000);
+        getPage().waitForTimeout(2000);
         int afterCountPoints = homePage.getProgressbarPointsNumber();
         int afterCountSideMenuPoints = homePage.getProgressbarSideMenuPointsNumber();
 
