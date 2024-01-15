ad87950aaa5f73d93602004dbbf4a984f46b8547
ArinaJur <66705803+arinajur@users.noreply.github.com>
2024-01-15 00:52:55 -0500

    Clean BaseTest, create LoggerUtils, TracingUtils

--- src/test/java/utils/TestData.java
+++ src/test/java/utils/TestData.java
@@ -2,8 +2,12 @@ package utils;
 
 public class TestData {
 
+      public final static String HOME_END_POINT = "/home";
+      public final static String SIGN_IN_END_POINT = "/sign-in";
       public static final String TEST_TUTOR_END_POINT = "/test-tutor";
+      public static final String MNEMONIC_CARDS_END_POINT = "/mnemonic-cards/";
+
       public static final String ONE = "1";
-      public static final String MNEMONIC_CARDS_PAGE_URL = "https://staging.traineracademy.org/mnemonic-cards/";
+
 
 }
