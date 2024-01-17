c062bd2860b50543799c5d97c468ad335daef671
laysan <laysansh@gmail.com>
2024-01-17 09:32:30 -0700

    RF method startTest in PreconditionPage

--- src/test/java/tests/FlashcardPacksTest.java
+++ src/test/java/tests/FlashcardPacksTest.java
@@ -16,6 +16,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class FlashcardPacksTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testUserCanMarkCardsForRechecking() {
 
