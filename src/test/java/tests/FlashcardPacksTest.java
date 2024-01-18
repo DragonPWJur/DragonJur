2a25471fc6ff34728b668dff773b23892cbbb600
verafes <verafes@gmail.com>
2024-01-17 21:31:42 -0800

    Ignored TC1349-02 testUserCanMarkCardsForRecheckingRandom

--- src/test/java/tests/FlashcardPacksTest.java
+++ src/test/java/tests/FlashcardPacksTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import pages.FlashcardsPackIDPage;
 import pages.HomePage;
@@ -14,6 +15,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class FlashcardPacksTest extends BaseTest {
 
+    @Ignore
     @Test
     public void testUserCanMarkCardsForRecheckingRandom() {
 
